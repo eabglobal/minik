40c2f5e1f11aaa1db298b024f8b298148a120b63
pdiazvargas <pdiazvargas@eab.com>
2019-01-28 16:54:42 -0500

    Updating version

--- minik/__init__.py
+++ minik/__init__.py
@@ -1,1 +1,8 @@
-__version__ = 0.0.0
+# -*- coding: utf-8 -*-
+"""
+    constants.py
+    :copyright: © 2019 by the EAB Tech team.
+    :license: Apache License, Version 2.0, see LICENSE for more details.
+"""
+
+__version__ = '0.1.dev'
